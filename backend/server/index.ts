efa3311fcebbfd938bb74871318c73165c7b3dd1
Victor Timi <victortimidev@gmail.com>
2024-03-05 12:16:46 +0100

    init: Initial commit

---
+++ backend/server/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import prisma from "@/lib/prisma";
+
+async function main() {
+  const app = express();
+
+  const test = await prisma.test.create({
+    data: {
+      title: "test title",
+      body: "test body",
+    },
+  });
+  console.log(test);
+
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+// main();
